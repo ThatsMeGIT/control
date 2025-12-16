79ce1dbb1bc7a245a594d5077e314b0d0c9bdfd0
Oshgnacknak <osh@oshgnacknak.de>
2025-12-16 19:15:14 +0100

    Allow turning power on and off

--- machines/src/lib.rs
+++ machines/src/lib.rs
@@ -450,8 +450,8 @@ pub trait MachineWithChannel: Send + Debug + Sync {
     fn get_machine_channel(&self) -> &MachineChannel;
     fn get_machine_channel_mut(&mut self) -> &mut MachineChannel;
 
-    fn update(&mut self, now: std::time::Instant);
-    fn mutate(&mut self, mutation: Value) -> Result<()>;
+    fn update(&mut self, now: std::time::Instant) -> Result<()>;
+    fn mutate(&mut self, value: Value) -> Result<()>;
 }
 
 impl<C> MachineApi for C
@@ -481,7 +481,9 @@ where
             self.act_machine_message(msg);
         }
 
-        self.update(now);
+        if let Err(e) = self.update(now) {
+            tracing::error!("Machine errored while updating: {}, ", e);
+        }
     }
 
     fn act_machine_message(&mut self, msg: MachineMessage) {
