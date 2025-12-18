356115f610af744f06382f57feb6a1f6e8affc26
Oshgnacknak <osh@oshgnacknak.de>
2025-12-18 22:08:03 +0100

    Fix mock-compile

--- machines/src/lib.rs
+++ machines/src/lib.rs
@@ -417,7 +417,6 @@ pub struct MachineChannel {
 }
 
 impl MachineChannel {
-
     pub fn new(machine_identification_unique: MachineIdentificationUnique) -> Self {
         let (sender, receiver) = smol::channel::unbounded();
 
@@ -426,16 +425,15 @@ impl MachineChannel {
             api_receiver: receiver,
             machine_identification_unique,
             main_sender: None,
-            namespace: None
+            namespace: None,
         }
     }
 }
 
 impl<E> NamespaceCacheingLogic<E> for MachineChannel
 where
-    E: CacheableEvents<E>
+    E: CacheableEvents<E>,
 {
-
     fn emit(&mut self, events: E) {
         let event = Arc::new(events.event_value());
         let cache_fn = events.event_cache_fn();
@@ -458,7 +456,7 @@ pub trait MachineWithChannel: Send + Debug + Sync {
 
 impl<C> MachineApi for C
 where
-    C: MachineWithChannel
+    C: MachineWithChannel,
 {
     fn api_get_sender(&self) -> Sender<MachineMessage> {
         self.get_machine_channel().api_sender.clone()
@@ -481,9 +479,8 @@ where
 
 impl<C> MachineAct for C
 where
-    C: MachineWithChannel
+    C: MachineWithChannel,
 {
-
     fn act(&mut self, now: Instant) {
         while let Ok(msg) = self.get_machine_channel_mut().api_receiver.try_recv() {
             self.act_machine_message(msg);
